d3cc300b4b1a03257002a92a9830b9e0aa76de93
Devdutt Thaware <Devdutt.Thaware@cmartsolutions.com>
2023-03-10 15:15:32 +0530

    Book Store commit 2

---
+++ Practice.BookStore/Practice.BookStore/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice.BookStore.Models;
+using Practice.BookStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View(data);
+        } 
+
+        public ViewResult GetBook(int id)
+        {
+            var data = _bookRepository.GetBookById(id);
+            return View(data);
+        }
+
+        public List<BookModel> SearchBooks(string bookName,string authorname)
+        {
+            return _bookRepository.SearchBook(bookName,authorname);
+        }
+    }
+}
--- Practice.BookStore/Practice.BookStore/Controllers/HomeController.cs
+++ Practice.BookStore/Practice.BookStore/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Practice.BookStore.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public ViewResult Index()
         {
             return View();
         }
